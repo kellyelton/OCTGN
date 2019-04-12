43b153b15bdc3e50bf098d1a17105531540233c4
Kelly Elton <its.the.doc@gmail.com>
2019-04-11 22:39:14 -0500

    Added InvertedBooleanConverter

---
+++ octgnFX/Octgn.Installer/Converters/InvertedBooleanConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Octgn.Installer.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InvertedBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            CultureInfo culture) {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            CultureInfo culture) {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+    }
+}
