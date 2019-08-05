d74b80e0f94317a3469d35d7692d4ce356d76007
Kelly Elton <its.the.doc@gmail.com>
2019-08-04 21:58:53 -0500

    DeleteDirectory step

---
+++ octgnFX/Octgn.Installer/Steps/DeleteDirectory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Octgn.Installer.Steps
+{
+    public class DeleteDirectory : Step
+    {
+        public DirectoryInfo Directory { get; }
+
+        public DeleteDirectory(string path) {
+            if (string.IsNullOrWhiteSpace(path)) throw new ArgumentNullException(nameof(path));
+
+            Directory = new DirectoryInfo(path);
+        }
+
+        public override async Task Execute(Context context) {
+            if (!Directory.Exists) return;
+
+            Directory.Delete(true);
+
+            await Task.Delay(100);
+
+            Directory.Refresh();
+
+            while (Directory.Exists) {
+                await Task.Delay(100);
+
+                Directory.Refresh();
+            }
+        }
+    }
+}
