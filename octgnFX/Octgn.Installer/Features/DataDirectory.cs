887396c2c43c0f20528f92c4f57beeb839ffc87a
Kelly Elton <its.the.doc@gmail.com>
2019-08-05 00:12:49 -0500

    Steps for DataDirectory

--- octgnFX/Octgn.Installer/Features/DataDirectory.cs
+++ octgnFX/Octgn.Installer/Features/DataDirectory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Octgn.Installer.Steps;
 
 namespace Octgn.Installer.Features
 {
@@ -9,5 +11,9 @@ namespace Octgn.Installer.Features
         public override string Name => "OCTGN Data Directory";
 
         public override string Description => "The OCTGN Data Directory. This is where all the games, cards, images, decks and user settings are stored.";
+
+        public override IEnumerable<Step> GetInstallSteps(Context context) {
+            yield return new CreateDirectory(context.DataDirectory);
+        }
     }
 }
